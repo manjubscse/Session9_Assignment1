a2cc684d1312304ca4bf0cca048bc7bc22e367aa
Manju B S <manjubscse@gmail.com>
2017-11-11 17:07:03 +0530

    Assignment9.1

---
+++ app/src/main/java/com/manjubs/SubMenuToChangeTextColor/ColourEnum.java
@@ -0,0 +1,14 @@
+package com.manjubs.SubMenuToChangeTextColor;
+
+/**
+ * Created by Manju on 27/09/2017.
+ */
+
+public class ColourEnum {
+
+    public enum AppColours{
+        Red,
+        Green,
+        Blue
+    }
+}
